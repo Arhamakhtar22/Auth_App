7674eccf1bb093f009ba8bf14aef92907e61f6e4
Arham Shoaib <arhamshoaib22@outlook.com>
2022-04-18 11:17:02 -0700

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,70 @@
 const express = require('express');
 const app = express();
 const User = require('./models/user');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
 
-app.get('/secert', (req, res) =>{
-    res.send('THIS IS SECERT!')
+mongoose.connect('mongodb://localhost:27017/authApp',{useNewUrlParser: true})
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN")
+    })
+    .catch(err => {
+        console.log("CONNECTION ERROR")
+        console.log(err)
+    })
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.urlencoded({ extended: true}));
+app.use(session({secret:'notagoodsecret'}));
+app.get('/', (req, res) => {
+    res.send('HOME PAGE');
+})
+
+app.get('/register', (req, res) =>{
+    res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+    const { password, username } = req.body;
+    const hash = await bcrypt.hash(password, 12); //hash the pass
+    const user = new User({ //storing the hash pass in database
+        username,
+        hashedPassword: hash
+    })
+    await user.save();
+    req.session.user_id = user._id;
+    res.redirect('/');
+})
+
+app.get('/login', (req, res) =>{
+    res.render('login')
+})
+
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    const user = await User.findOne({username: username}); //checl if we can find the username
+    const valid = await bcrypt.compare(password, user.hashedPassword); //compare the pass entered w hashed pass
+    if (valid) {
+        req.session.user_id = user._id; //if u succesfully login we will store your userid in session
+        res.redirect('/secret');
+    } else {
+        res.redirect('/login');
+    }
+})
+
+app.get('/secret', (req, res) =>{
+    if (req.session.user_id){
+        res.send('THIS IS SECRET!')
+    } else{
+        res.redirect('/login')
+    }
+    
 })
 
 app.listen(3000, () => {
     console.log("SERVING YOUR APP!")
 })
+//Fawiat@123sucks
